c0f27d0a7838d1b11895adae471d378f1b1ab263
James Adam <jcadam@startmail.com>
2024-05-06 16:03:29 -0800

    some more changes

--- init.el
+++ init.el
@@ -5,6 +5,14 @@
 (package-initialize)
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
+(dolist (package '(use-package))
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(use-package dracula-theme
+	     :ensure t)
+
 (load-theme 'dracula t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -12,7 +20,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(poetry general evil-visual-mark-mode lsp-treemacs company rustic lsp-pyright lsp-mode rust-mode dracula-theme speedbar-git-respect treemacs treeview)))
+   '(typescript poetry general evil-visual-mark-mode lsp-treemacs company rustic typescript-mode lsp-pyright lsp-mode rust-mode dracula-theme speedbar-git-respect treemacs treeview)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -20,12 +28,19 @@
  ;; If there is more than one, they won't work right.
  )
 
-(require 'rust-mode)
-(use-package rustic)
-(require 'evil)
-(require 'python)
+(use-package rust-mode
+	     :ensure t)
+(use-package rustic
+	     :ensure t)
+(use-package evil
+	     :ensure t)
+(use-package python
+	     :ensure t)
+(use-package typescript-mode
+	     :ensure t)
 (evil-mode 1)
 (use-package lsp-mode
+  :ensure t	     
   :config
   (define-key lsp-mode-map (kbd "C-c l") lsp-command-map))
 ;; Set LSP mode
