b5c0642a8e8862b32111cab9ca4f0c1ae65a413c
rhinoman <jamesadam@certuscore.com>
2025-05-20 18:25:14 -0800

    updates

--- init.el
+++ init.el
@@ -2,9 +2,14 @@
 (add-to-list 'package-archives
 	     '("MELPA" .
 	       "http://melpa.org/packages/"))
-(add-to-list 'load-path "~/.emacs.d/copilot.el")
-(require 'copilot)	     
 (package-initialize)
+;; Install use-package if not already installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
 (xterm-mouse-mode 1)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
@@ -12,9 +17,26 @@
   (unless (package-installed-p package)
     (package-install package)))
 
+;; Enable defer and ensure by default for use-package
+;; Keep auto-save/backup files separate from source code:  https://github.com/scalameta/metals/issues/1027
+(setq use-package-always-defer t
+      use-package-always-ensure t
+      backup-directory-alist `((".*" . ,temporary-file-directory))
+      auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
 (use-package no-littering
   :ensure t)
+
+(use-package transient
+  :ensure t
+  :demand t)
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+	 ("C-c C-e" . markdown-do)))
 
 (use-package dracula-theme
 	     :ensure t)
@@ -26,7 +48,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(json-rpc editorconfig treemacs-evil groovy-mode flycheck-rust flycheck evil ttl-mode python-ts poetry-mode python-ts-mode poetry-ts-mode treesit-auto company-capf company-anaconda poetry general evil-visual-mark-mode lsp-treemacs company rustic lsp-pyright lsp-mode rust-mode dracula-theme speedbar-git-respect treemacs treeview)))
+   '(clojure-ts-mode company-anaconda dracula-theme editorconfig
+		     flycheck-rust groovy-mode lsp-metals lsp-pyright
+		     lsp-ui no-littering poetry rustic sbt-mode
+		     treemacs-evil treesit-auto ttl-mode yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -43,6 +68,9 @@
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
 (use-package company
+  :hook (scala-mode . company-mode)
+  :config
+  (setq lsp-completion-provider :capf)
   :ensure t)
 (use-package company-anaconda
   :ensure t)
@@ -52,6 +80,18 @@
   :ensure t)
 (use-package rustic
   :ensure t)
+(use-package scala-mode
+  :ensure t
+  :interpreter ("scala" . scala-mode))
+(use-package sbt-mode
+  :ensure t
+  :commands sbt-start sbt-command
+  :config
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+  (setq sbt:program-options '("-Dsbt.supershell=false")))
 (use-package groovy-mode
   :ensure t)
 (use-package evil
@@ -69,7 +109,7 @@
   :mode (("\\.ttl\\'" . ttl-mode))
   :ensure t)
 
-(use-package typescript-ts
+(use-package typescript-ts-mode
   :mode (("\\.ts\\'" . typescript-ts-mode)
          ("\\.tsx\\'" . tsx-ts-mode))
   :after lsp)
@@ -78,17 +118,26 @@
   :ensure t	     
   :config
   (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
-  :hook ((rust-mode
-	  rust-ts-mode
-	  typescript-mode
-	  typescript-ts-mode
-	  javascript-ts-mode
-	  tsx-mode
-	  tsx-ts-mode) . lsp)
-  :commands lsp)
+  (setq lsp-prefer-flymake nil)
+  (setq lsp-keep-workspace-alive nil)
+  :hook (rust-mode . lsp)
+	(rust-ts-mode . lsp)
+	(scala-mode . lsp)
+	(typescript-mode . lsp)
+	(typescript-ts-mode . lsp)
+	(javascript-ts-mode . lsp)
+	(tsx-mode . lsp)
+	(tsx-ts-mode . lsp)
+	(lsp-mode . lsp-lens-mode))
 
 (setq lsp-pyright-use-library-code-for-types nil)
 (setq python.analysis.typeCheckingMode "basic")
+(use-package lsp-metals
+  :ensure t)
+(use-package lsp-ui
+  :ensure t)
+(use-package yasnippet
+  :ensure t)
 (use-package lsp-pyright
   :ensure t
   :hook (python-ts-mode . (lambda ()
@@ -96,7 +145,15 @@
 			    (lsp))))
 
 (eval-after-load "company"
- '(add-to-list 'company-backends '(company-anaconda :with company-capf)))
+  '(add-to-list 'company-backends '(company-anaconda :with company-capf)))
+(use-package posframe
+  :ensure t)
+(use-package dap-mode
+  :ensure t
+  :hook
+  (lsp-mode . dap-mode)
+  (lsp-mode . dap-ui-mode))
+
 (setq lsp-auto-guess-root t)
 (setq lsp-prefer-capf t)
 (setq lsp-diagnostics-provider :flycheck)
@@ -104,9 +161,6 @@
 ;(add-to-list 'lsp-language-id-configuration '(js-jsx-mode . "javascriptreact"))
 
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
-(add-hook 'prog-mode-hook 'copilot-mode)
-(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
-(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
 (with-eval-after-load 'rust-ts-mode
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 (add-hook 'rust-ts-mode-hook #'rustic-mode)
